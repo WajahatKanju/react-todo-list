7dc36d5e684c64546e03da5fc1a50226d8ef0607
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-11-07 22:35:09 +0500

    add time functionality

--- src/todo/TODO.js
+++ src/todo/TODO.js
@@ -3,11 +3,61 @@ import "./todo.css";
 import { ACTIONS } from "./TodoList";
 
 function TODO({ todo, dispatch }) {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  function parsetime(hours, minues, seconds) {
+    let AMPM = hours > 12 ? "PM" : "AM";
+    hours = hours > 12 ? hours - 12 : hours;
+    if (hours === 12) {
+      AMPM = "AM";
+    } else if (hours === 0) {
+      hours += 12;
+      AMPM = "PM";
+    }
+
+    return `${String(hours).padStart(2, "0")}:${String(minues).padStart(
+      2,
+      "0"
+    )} ${AMPM}`;
+  }
+
+  const parseDate = () => {
+    if (todo.date === "") {
+      return;
+    } else {
+      const date = new Date(todo.date);
+      const DD = String(date.getDate()).padStart(2, "0");
+      const MM = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+      const YYYY = date.getFullYear();
+      const hh = date.getHours();
+      const mm = date.getMinutes();
+      const ss = date.getSeconds();
+      return DD + "-" + monthNames[MM-1] + "-" + YYYY + " " + parsetime(hh, mm, ss);
+    }
+  };
+
   return (
     <>
       <div className="todo">
         <div className="todo-text">
           <p className={todo.complete ? "complete" : ""}>{todo.value}</p>
+          {console.log(todo)}
+          <p className={todo.complete ? "hidden" : ""}>
+            <small> {parseDate()}</small>
+          </p>
         </div>
         <div className="todo-functions">
           <button
@@ -19,7 +69,9 @@ function TODO({ todo, dispatch }) {
             {/* <i className="fa-solid fa-check"></i> */}
             <i
               className={
-                todo.complete ? "fa-solid fa-check dull" : "fa-solid fa-check checked"
+                todo.complete
+                  ? "fa-solid fa-check dull"
+                  : "fa-solid fa-check checked"
               }
             ></i>
           </button>
--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -13,7 +13,7 @@ function TODOS() {
   function reducer(todos, action) {
     switch (action.type) {
       case ACTIONS.ADD:
-        return [...todos, newTodo(action.payload.value)];
+        return [...todos, newTodo(action.payload.value, action.payload.date)];
       case ACTIONS.DELETE:
         return todos.filter((todo) => {
           return todo.id !== action.id;
@@ -21,7 +21,6 @@ function TODOS() {
       case ACTIONS.COMPLETE:
         return todos.map((todo) => {
           if (todo.id === action.id) {
-            console.log(todo);
             return { ...todo, complete: !todo.complete };
           } else {
             return todo;
@@ -34,15 +33,22 @@ function TODOS() {
 
   const [todos, dispatch] = React.useReducer(reducer, []);
   const [todo, setTodo] = React.useState("");
+  const [dateTime, setDateTime] = React.useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    console.log(dateTime);
+
     if (todo === "") return;
-    dispatch({ type: ACTIONS.ADD, payload: { value: todo } });
+    dispatch({ type: ACTIONS.ADD, payload: { value: todo, date: dateTime } });
     setTodo("");
+    setDateTime("");
   }
 
-  function newTodo(value) {
-    return { id: Date.now(), value: value, complete: false };
+  function newTodo(value, date) {
+
+    return { id: Date.now(), value: value, complete: false, date: date };
   }
 
   return (
@@ -53,13 +59,28 @@ function TODOS() {
       })}
 
       <form onSubmit={handleSubmit}>
-       
+        <div className="wrapper">
+          <input
+          value={dateTime}
+            className="time"
+            name="datetime"
+            id="datetime"
+            onChange={(e) => {
+              setDateTime(e.target.value);
+            }}
+            type="datetime-local"
+          />
+          <label id="datetime-label" htmlFor="datetime">
+            Set datetime OPTIONAL
+          </label>
+        </div>
+
         <input
           className="task"
           type="text"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
-          required
+          onBlur={(e) => (e.target.required = true)}
         />
       </form>
     </div>
